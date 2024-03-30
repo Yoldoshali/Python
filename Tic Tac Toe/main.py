01fda947f005a10af71aa1a3e66e312d95f8614d
Yoldoshali Esonaliyev <86107814+Yoldoshali@users.noreply.github.com>
2024-03-30 14:01:08 +0500

    Update main.py

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -1,13 +1,21 @@
-board = [" "]*9
+"""Basic Tic Toe Game.Code has been rated at 9.55/10"""
+
+board = [" "] * 9
 
 
-def display_board(board):
-    print(" | ".join(board[0:3]))
-    print(" | ".join(board[3:6]))
-    print(" | ".join(board[6::]))
+def display_board(board_updated):
+    """
+    Display the updated board
+    """
+    print(" | ".join(board_updated[0:3]))
+    print(" | ".join(board_updated[3:6]))
+    print(" | ".join(board_updated[6::]))
 
 
 def player_input():
+    """
+    Display user choice as an integer
+    """
     user_input = input("Choose a number between 0 and 8: ")
     if user_input.isdigit():
         if 8 >= int(user_input) >= 0:
@@ -20,51 +28,60 @@ def player_input():
         player_input()
 
 
-def place_marker(board, marker, position):
-    board[position] = marker
-    return board
+def place_marker(updated_board, marker, position):
+    """
+    Update the board after user input the position
+    """
+    updated_board[position] = marker
+    return updated_board
 
 
-def win_check(board, mark):
+def win_check(updated_board, mark):
+    """
+    Check if the player has won and return True otherwise return False
+    """
     for i in range(0, 3):
-        if board[i] == board[i+3] == board[i+6] == mark:
+        if updated_board[i] == updated_board[i + 3] == updated_board[i + 6] == mark:
             return True
-    for i in range(0,7, 3):
-        if board[i] == board[i + 1] == board[i + 2] == mark:
+    for i in range(0, 7, 3):
+        if updated_board[i] == updated_board[i + 1] == updated_board[i + 2] == mark:
             return True
-    if board[0] == board[4] == board[8] == mark:
+    if updated_board[0] == updated_board[4] == updated_board[8] == mark:
         return True
-    if board[3] == board[5] == board[7] == mark:
-        return True
-    return False
+
+    return updated_board[3] == updated_board[5] == updated_board[7] == mark
 
 
-def space_check(board, position):
-    if " " in board[position]:
-        return True
-    return False
+def space_check(current_board, position):
+    """
+    Check if the position is taken. Return True if it is available otherwise return False
+    """
+    return " " in current_board[position]
 
 
-def full_board_check(board):
-    if " " not in board:
-        return True
-    return False
+def full_board_check(current_board):
+    """
+    Check if the board is full. Return True if it is full otherwise return False
+    """
+    return " " not in current_board
 
 
 def replay():
-    play_again = input("\nWould you like to play again?(Y/N): ")
-    if play_again.lower() == "y":
+    """
+    Check if the user wants to play again. Return True if the user wants to play again otherwise return False
+    """
+    play_again = input("\nWould you like to play again?(y/n): ").lower()
+    if play_again == "y":
         return True
-    else:
-        return False
+    return False
 
 
 print('Welcome to Tic Tac Toe!')
-game_on = True
-play = True
-while play:
+GAME_ON = True
+PLAY = True
+while PLAY:
     if not full_board_check(board):
-        while game_on:
+        while GAME_ON:
             print("\nPlayer1:")
             display_board(board)
             player_1 = player_input()
@@ -74,9 +91,9 @@ while play:
             board = place_marker(board, "X", player_1)
             if win_check(board, "X"):
                 print("\nPlayer 1 wins!")
-                game_on = False
+                GAME_ON = False
             else:
-                print("\n"*10)
+                print("\n" * 10)
                 display_board(board)
                 print("Player2:")
                 player_2 = player_input()
@@ -86,11 +103,9 @@ while play:
                 board = place_marker(board, "O", player_2)
                 if win_check(board, "O"):
                     print("\nPlayer 2 wins!")
-                    game_on = False
-                print("\n"*10)
+                    GAME_ON = False
+                print("\n" * 10)
     else:
         print("\n\n Draw")
 
-    play = replay()
-
-
+    PLAY = replay()
